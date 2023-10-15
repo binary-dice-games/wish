d7793b45aa990e9398c880abc51115a26ef8c0dc
Carlos Lopez <carloslopezmdez@gmail.com>
2023-10-15 18:13:30 +0200

    .

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,3 +1,16 @@
+#include "src/wish.hpp"
+
+using namespace bdg::wish;
+
 int main() {
+  ostream out(std::cout);
+  out << "Hello world" << std::endl;
+
+
+  std::string line;
+  istream in(std::cin);
+  std::getline(in, line);
+  out << "Name: " << line << std::endl;
+
   return 0;
 }
